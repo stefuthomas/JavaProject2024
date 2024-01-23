0c48719b1788c7f658028d4efd74d58fbc7426aa
stefuthomas <142782594+stefuthomas@users.noreply.github.com>
2024-01-23 19:09:05 +0200

    Added methods for cruise control.

--- Module2/Module2.1/src/Car.java
+++ Module2/Module2.1/src/Car.java
@@ -6,17 +6,25 @@ public class Car {
     private String typeName;
     private float tankCapacity;
     private float topSpeed;
+    private boolean cruiseControlOn;
     private int targetSpeed;
+
     public Car(String typeName) {
-        speed = 0; gasolineLevel = 0;
+        speed = 0;
+        gasolineLevel = 0;
         this.typeName = typeName;
+        cruiseControlOn = false;
+        targetSpeed = 0;
     }
 
     public Car(String typeName, float tankCapacity, float topSpeed) {
-        speed = 0; gasolineLevel = 0;
+        speed = 0;
+        gasolineLevel = 0;
         this.tankCapacity = tankCapacity;
         this.topSpeed = topSpeed;
         this.typeName = typeName;
+        cruiseControlOn = false;
+        targetSpeed = 0;
     }
 
     public void accelerate() {
@@ -25,6 +33,7 @@ public class Car {
         else
             speed = 0;
     }
+
     void decelerate(int amount) {
         if (gasolineLevel > 0) {
             if (amount > 0)
@@ -32,44 +41,99 @@ public class Car {
         } else
             speed = 0;
     }
+
     float getSpeed() {
         return speed;
     }
+
     String getTypeName() {
         return typeName;
     }
+
     void fillTank() {
         gasolineLevel = 100;
     }
+
     float getGasolineLevel() {
         return gasolineLevel;
     }
 
-    public void cruiseControl(boolean on) {
-        int targetSpeed = setTargetSpeed();
-        if (on && targetSpeed <= topSpeed) {
-            while (speed < targetSpeed) {
-                accelerate();
+    public void cruiseControl() {
+        if (cruiseControlOn) {
+            if (targetSpeed > speed) {
+                while (speed < targetSpeed && gasolineLevel > 0) {
+                    accelerate();
+                    System.out.println("Cruise control on: speed is " + speed + " km/h");
+                }
+            } else if (targetSpeed < speed) {
+                while (speed > targetSpeed && gasolineLevel > 0) {
+                    decelerate(10);
+                    System.out.println("Cruise control on: speed is " + speed + " km/h");
+                }
+            }
+            if (gasolineLevel == 0) {
+                System.out.println("Out of gas. Cruise control turned off.");
+                cruiseControlOn = false;
             }
         } else {
-            while (speed > 0) {
-                decelerate(10);
-            }
-        }
-        if (targetSpeed >= topSpeed) {
-            System.out.println("Target speed is too high!");
+            System.out.println("Cruise control off");
         }
     }
 
-    private int setTargetSpeed() {
+    public void setCruiseControlSpeed(int minTargetSpeed, int maxTargetSpeed) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter target speed: ");
+        System.out.println("Enter target speed between " + minTargetSpeed + " and " + maxTargetSpeed + ": ");
         targetSpeed = scanner.nextInt();
+        if (targetSpeed < minTargetSpeed || targetSpeed > maxTargetSpeed) {
+            System.out.println("Invalid target speed. Cruise control turned off.");
+            cruiseControlOn = false;
+        } else {
+            cruiseControlOn = true;
+        }
+    }
+
+    public int getTargetSpeed() {
         return targetSpeed;
+    }
+
+    public boolean isCruiseControlOn() {
+        return cruiseControlOn;
     }
 }
 
-
 class CarDriver {
-    
+
+    public static void main(String[] args) {
+        Car myCar;
+
+        myCar = new Car("Toyota Corolla", 50, 200);
+        myCar.fillTank();
+
+        for (int i = 0; i < 6; i++) {
+            myCar.accelerate();
+            System.out.println(myCar.getTypeName() + ": speed is " + myCar.getSpeed() + " km/h");
+        }
+
+        while (myCar.getSpeed() > 0) {
+            myCar.decelerate(15);
+            System.out.println(myCar.getTypeName() + ": speed is " + myCar.getSpeed() + " km/h");
+        }
+
+
+        while (true) {
+            System.out.println("1) to use cruise control");
+            System.out.println("2) to view current speed");
+            System.out.println("3) to exit");
+            Scanner scanner = new Scanner(System.in);
+            int choice = scanner.nextInt();
+            if (choice == 1) {
+                myCar.setCruiseControlSpeed(60, 150);
+                myCar.cruiseControl();
+            } else if (choice == 2) {
+                System.out.println("Current speed is " + myCar.getSpeed() + " km/h");
+            } else if (choice == 3) {
+                break;
+            }
+        }
+    }
 }
