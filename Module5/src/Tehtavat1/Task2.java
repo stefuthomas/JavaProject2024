3936cf297e9b7414bfbea368521f22a10237dc49
stefu <stefanos.thomas@metropolia.fi>
2024-02-08 22:17:32 +0200

    Task 2 ready

--- Module5/src/Tehtavat1/Task2.java
+++ Module5/src/Tehtavat1/Task2.java
@@ -1,12 +1,15 @@
 package Tehtavat1;
+import org.w3c.dom.ls.LSOutput;
 
 import java.util.ArrayList;
+import java.util.Scanner;
+
 class Calculator extends Thread {
     private int start;
     private int end;
     private int[] numbers;
     private int sum;
-    Calculator(int start, int end, int[] numbers) {
+    public Calculator(int start, int end, int[] numbers) {
         this.start = start;
         this.end = end;
         this.numbers = numbers;
@@ -33,33 +36,40 @@ class Calculator extends Thread {
 }
 public class Task2 {
     public static void main(String[] args) {
-        int size = Runtime.getRuntime().availableProcessors() * 1000;
-        int[] numbers = new int[size];
+        int[] numbersToCalculate;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Numbers: ");
+        int size = scanner.nextInt();
+        numbersToCalculate = new int[size];
+
         for (int i = 0; i < size; i++) {
-            numbers[i] = (int) (Math.random()*100);
+            numbersToCalculate[i] = (int) (Math.random() * 100);
         }
-
         ArrayList<Calculator> calculators = new ArrayList<>();
-        calculators.add(new Calculator(0, size/4, numbers));
-        calculators.add(new Calculator(size/4, size/2, numbers));
-        calculators.add(new Calculator(size/2, numbers.length-1, numbers));
+        calculators.add(new Calculator(0, size / 4 - 1, numbersToCalculate));
+        calculators.add(new Calculator(size / 4, size / 2 - 1, numbersToCalculate));
+        calculators.add(new Calculator(size / 2, size / 4 * 3 - 1, numbersToCalculate));
+        calculators.add(new Calculator(size / 4 * 3, size - 1, numbersToCalculate));
 
-        for (Calculator c : calculators) {
-            c.start();
+        for (Calculator calculator : calculators) {
+            calculator.start();
         }
 
-        try {
-            for (Calculator c : calculators) {
-                c.join();
+        for (Calculator calculator : calculators) {
+            try {
+                calculator.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        int threadSum = 0;
-        for (Calculator c : calculators) {
-            threadSum += c.getSum();
         }
 
-        System.out.println("Sum of numbers: " + threadSum);
+        int threadedSum = 0;
+
+        for (Calculator calculator : calculators) {
+            System.out.println("Threadded calculator sum: " + calculator.getSum());
+            threadedSum += calculator.getSum();
+        }
+
+        System.out.println("Threaded sum: " + threadedSum);
     }
 }
