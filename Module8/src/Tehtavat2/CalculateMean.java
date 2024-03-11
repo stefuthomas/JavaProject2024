c4f73195fd079f1f3e89d754aeb11c1f4e2ccead
stefu <stefanos.thomas@metropolia.fi>
2024-03-11 19:46:27 +0200

    CalculateMean is calculating the mean of the array and printing in functional way

---
+++ Module8/src/Tehtavat2/CalculateMean.java
@@ -0,0 +1,16 @@
+package Tehtavat2;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class CalculateMean {
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(32, 14, 63, 25, 74, 94, 38, 28);
+        double mean = numbers.stream()
+                .mapToInt(Integer::intValue)
+                .average()
+                .orElse(Double.NaN);
+        System.out.println(mean);
+    }
+}
