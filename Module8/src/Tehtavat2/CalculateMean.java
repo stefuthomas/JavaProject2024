ba495e3b2b3c777126313a8f4d7c13554569ead2
stefuthomas <142782594+stefuthomas@users.noreply.github.com>
2024-03-12 14:11:43 +0200

    refactored the sort method

--- Module8/src/Tehtavat2/CalculateMean.java
+++ Module8/src/Tehtavat2/CalculateMean.java
@@ -6,10 +6,11 @@ import java.util.List;
 public class CalculateMean {
     public static void main(String[] args) {
         List<Integer> numbers = Arrays.asList(32, 14, 63, 25, 74, 94, 38, 28);
-        double mean = numbers.stream()
+        System.out.println(
+            numbers.stream()
                 .mapToInt(Integer::intValue)
                 .average()
-                .orElse(Double.NaN);
-        System.out.println(mean);
+                .orElse(Double.NaN)
+        );
     }
 }
